d9f850dd94f37e74ca2e84dd7d73bbb4728661f4
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-05-09 18:06:35 -0300

    Atualiza o arquivo Makefile.am

---
+++ include/minha_lib.hpp
@@ -0,0 +1,9 @@
+#ifndef MINHA_LIB_HPP
+#define MINHA_LIB_HPP
+
+namespace minhalib {
+    int soma(int a, int b);
+}
+
+#endif // MINHA_LIB_HPP
+
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "minha_lib.hpp"
+
+int main() {
+    int resultado = minhalib::soma(3, 4);
+    std::cout << "Resultado: " << resultado << std::endl;
+    return 0;
+}
+
