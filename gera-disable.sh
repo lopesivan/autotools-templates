9cee2e05b10fe63daec070863e39da58d742ee80
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-05-12 13:18:25 -0300

    Atualiza o arquivo USO

--- gera-disable.sh
+++ gera-disable.sh
@@ -13,14 +13,28 @@ define_name=$(echo "$option" | tr '[:lower:]' '[:upper:]')
 
 DOLAR='$'
 cat <<EOF
-dnl --disable-${option} => adiciona #define ${define_name} 1
+dnl --disable-${option} => adiciona #define ${define_name} 0
+AC_DEFINE([${define_name}], [1], [${description}])
 AC_ARG_ENABLE([${option}],
   AS_HELP_STRING([--disable-${option}], [${description}]),
-  [test "x${DOLAR}enable_${option}" = xno && AC_DEFINE([${define_name}], [1],
+  [test "x${DOLAR}enable_${option}" = xno && AC_DEFINE([${define_name}], [0],
                [Define para ${description}])])
 
 EOF
 
 echo "Adicione o bloco acima ao configure.ac!"
 
+# Gera o bloco C para usar o #define no código-fonte
+cat <<EOF
+
+/*
+ * #ifdef ${define_name}
+ *   puts("${define_name} LIGADO");
+ * #else
+ *   puts("${define_name} DESLIGADO");
+ * #endif
+ *
+ * Adicione o bloco acima ao seu código C para ativar o comportamento opcional.
+ */
+EOF
 exit 0
