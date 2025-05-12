a64eb5abee37f59fde453af0d9a22efcca570c49
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-05-12 13:32:17 -0300

    Atualiza o arquivo USO

---
+++ gera-with.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Script: gera-with-opcao.sh
+if [[ $# -lt 2 ]]; then
+    echo "Uso: $0 <nome_da_opcao> <descricao>"
+    echo "Exemplo: $0 openssl 'usa biblioteca OpenSSL do diretório especificado'"
+    exit 1
+fi
+
+option="$1"
+description="$2"
+define_name=$(echo "$option" | tr '[:lower:]' '[:upper:]')
+
+DOLAR='$'
+cat <<EOF
+dnl --with-${option} => adiciona #define WITH_${define_name} e define variável
+AC_ARG_WITH([${option}],
+  AS_HELP_STRING([--with-${option}=@<:@DIR@:>@], [${description}]),
+  [
+    if test "x${DOLAR}with_${option}" != xno; then
+      AC_DEFINE([WITH_${define_name}], [1], [Define se ${option} está habilitado])
+      ${define_name}_PATH="${DOLAR}with_${option}"
+      AC_SUBST([${define_name}_PATH])
+    fi
+  ])
+
+EOF
+
+echo "Adicione o bloco acima ao configure.ac!"
