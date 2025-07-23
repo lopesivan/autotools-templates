f03e745df7e2c20fb34e3ecf46eef72de8120427
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-07-23 02:56:42 -0300

    Atualiza o arquivo configure.ac

--- autogen.sh
+++ autogen.sh
@@ -7,4 +7,19 @@ autoconf
 
 #touch NEWS README AUTHORS ChangeLog COPYING
 #autoreconf -i -v && ./configure && make
+
+# Verificar se foi bem-sucedido
+if [ $? -eq 0 ]; then
+    echo ""
+    echo "Configuração gerada com sucesso!"
+    echo "Agora execute:"
+    echo "  ./configure"
+    echo "  make"
+    echo ""
+    echo "Opções úteis do configure:"
+    echo "  --enable-debug     Habilitar modo debug"
+else
+    echo "Erro ao gerar configuração!"
+    exit 1
+fi
 exit 0
