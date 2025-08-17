b58b74f5bf15b8e8fc5f91752255037e2fce22ca
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-08-17 10:47:13 -0300

    :style: Atualiza o estilo do arquivo autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-set -e
-aclocal
-libtoolize --force
-# autoheader
-autoconf
-automake --add-missing --foreign
+mkdir -p build-aux m4
 
+libtoolize --install --copy # no macOS use: glibtoolize --install --copy
+aclocal -I m4
+autoconf
+automake --add-missing --copy
+
+./configure && make && make -C src run
 exit 0
