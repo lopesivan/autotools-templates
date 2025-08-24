3d710a6bb91891845a15ffe0b9e217d06def29c7
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-08-24 03:10:01 -0300

    :style: Atualiza o estilo do arquivo gera-with.sh

--- gera-with.sh
+++ gera-with.sh
@@ -15,18 +15,21 @@ DOLAR='$'
 cat <<EOF
 dnl --with-${option} => adiciona #define WITH_${define_name} e define variável
 AC_ARG_WITH([${option}],
-  AS_HELP_STRING([--with-${option}=@<:@DIR@:>@], [${description}]),
+  AS_HELP_STRING([--with-${option}=@<:@DIR@:>@], [$description]),
   [
-    if test "x${DOLAR}with_${option}" != xno; then
-      AC_DEFINE([WITH_${define_name}], [1], [Define se ${option} está habilitado])
-      ${define_name}_PATH="${DOLAR}with_${option}"
-      AC_SUBST([${define_name}_PATH])
+    if test "x${DOLAR}with_${option}" != "xno" && test -n "${DOLAR}with_${option}"; then
+      AC_DEFINE([WITH_${define_name}], [1], [Define se ${define_name} está habilitado])
+      ${define_name}_PATH="${DOLAR}with_openssl"
+      USE_${define_name}=true
+    else
+      USE_${define_name}=false
     fi
-  ])
+  ],
+  [USE_${define_name}=false]
+)
 
-dnl \`WITH_${define_name}' do Makefile.am.
-AM_CONDITIONAL(WITH_${define_name}, [1])
-
+AM_CONDITIONAL([WITH_${define_name}], [test "x\$USE_${define_name}" = "xtrue"])
+AC_SUBST([${define_name}_PATH])
 EOF
 
 echo "Adicione o bloco acima ao configure.ac!" >&2
