e9415bb5f4845525beeb7dff8da3fc128be9b5be
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-08-24 03:44:03 -0300

    :style: Atualiza o estilo do arquivo src/main.c

--- src/main.c
+++ src/main.c
@@ -1,25 +1,42 @@
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
+
+#include <gtk/gtk.h>
 #include <stdio.h>
 
-int main(void) {
-  printf("Autor: %s\n", AUTHOR_NAME);
-  printf("Versão: %s\n", PACKAGE_VERSION);
-  printf("Porta: %d\n", DEFAULT_PORT);
+static void on_button_clicked(GtkWidget *widget, gpointer data) {
+  g_print("Botão clicado!\n");
+}
 
-#ifdef DEBUG
-  puts("Compilado com DEBUG!");
-#endif
+int main(int argc, char *argv[]) {
+  GtkWidget *window;
+  GtkWidget *button;
 
-#ifdef ABOBRINHA
-  puts("Compilado com ABOBRINHA!");
-#endif
+  // Inicializa o GTK
+  gtk_init(&argc, &argv);
 
-  if (SECURITY)
-    puts("SECURITY LIGADO");
-  else
-    puts("SECURITY DESLIGADO");
+  // Cria uma nova janela
+  window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  gtk_window_set_title(GTK_WINDOW(window), "Exemplo GTK");
+  gtk_window_set_default_size(GTK_WINDOW(window), 300, 200);
+  gtk_container_set_border_width(GTK_CONTAINER(window), 10);
+
+  // Conecta o evento de fechar a janela
+  g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+  // Cria um botão e conecta o sinal de clique
+  button = gtk_button_new_with_label("Clique aqui");
+  g_signal_connect(button, "clicked", G_CALLBACK(on_button_clicked), NULL);
+
+  // Adiciona o botão à janela
+  gtk_container_add(GTK_CONTAINER(window), button);
+
+  // Exibe tudo
+  gtk_widget_show_all(window);
+
+  // Entra no loop principal do GTK
+  gtk_main();
 
   return 0;
 }
