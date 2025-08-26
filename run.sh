3b9977030c1e3e64026d6804b36a801488293143
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-08-26 01:28:36 -0300

    :style: Atualiza o estilo do arquivo run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,15 @@
-sh ./autogen.sh
-./configure
-make
+#!/usr/bin/env bash
 
+BAKEFILE_HOME=$HOME/wx/linux-wx-3.2.4
+ACLOCAL_HOME=$HOME/wx/wxWidgets-3.2.4-linux/build/aclocal
+
+PYENV_VERSION=2.7.18 \
+    $BAKEFILE_HOME/bin/bakefile -f autoconf hello.bkl
+
+#mkdir -p m4 && mv autoconf_inc.m4 m4/
+PYENV_VERSION=2.7.18 \
+    $BAKEFILE_HOME/bin/bakefilize --copy &&
+    aclocal -I $ACLOCAL_HOME &&
+    autoconf
+
+exit 0
